50bac9d3907d6ba8bb6ce52d6804f34e6fd96a9f
shaarvani-kavula <123433000+shaarvani-kavula@users.noreply.github.com>
2023-01-23 20:52:24 -0600

    Add files via upload

---
+++ postprocessor.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 23 11:05:36 2023
+
+@author: shaar
+"""
+
+import pandas as pd
+import spacy
+import numpy as np
+
+
+def get_unstructured_text(text_col):
+    pass
+
+
+def tokenize_text(text_col):
+    df['data'] = text_col
+    df['data'] = lowercase()
+    df['data'] = remove_punctuation()
+    df['data'] = remove_numbers()
+    df['data'] = stopwords()
+    df['data'] = 
+    return text_col
+    
+
+
+def build_wordcloud(freq_dict, stop_words):
+    pass
+
+
+def find_topics():
+    pass
+        
+
+def topic_tagging():
+    pass
+
+
+def sentiment_analysis():
+    pass
+
+if __name__ == "__main__":
+    
+    filepath = ("C:/Users/shaar/OneDrive/Documents/Reddit-NLP/"
+                "postprocessor/one-million-reddit-confessions-sample.csv")
+    df = pd.read_csv(filepath)
+    df['lemma'] = tokenize_text(df['title'])
+    
