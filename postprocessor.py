bfce750374032bce8f49fa9239a2ed154c00a917
shaarvani-kavula <123433000+shaarvani-kavula@users.noreply.github.com>
2023-01-24 15:50:09 -0600

    Update postprocessor.py

--- postprocessor.py
+++ postprocessor.py
@@ -8,6 +8,18 @@ Created on Mon Jan 23 11:05:36 2023
 import pandas as pd
 import spacy
 import numpy as np
+import nltk
+
+from collections import Counter
+from itertools import chain
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from wordcloud import WordCloud
+from matplotlib import pyplot as plt
+
+stop = stopwords.words('english')
+lemmatizer = WordNetLemmatizer()
+
 
 
 def get_unstructured_text(text_col):
@@ -22,10 +34,24 @@ def tokenize_text(text_col):
     df['data'] = df['data'].apply(lambda x: ' '.join([word for word in x.split() if word not in (stop)]))
     df['data'] = df['data'].apply(lambda x: [lemmatizer.lemmatize(y) for y in x.split()])
     return df['data']
+    
 
 
-def build_wordcloud(freq_dict, stop_words):
-    pass
+def build_wordcloud(lemma_column, stop_words=None):
+    text = WordCloud().generate(lemma_column.to_string())
+    plt.imshow(text)
+    plt.axis("off")
+    plt.show()
+    
+    
+def generate_ngrams(text_col, n=2):
+    bigrams_col = text_col.apply(lambda row: list(nltk.ngrams(row, n)))   
+    bigrams = bigrams_col.tolist()
+    bigrams = list(chain(*bigrams))
+    bigrams = [(x.lower(), y.lower()) for x,y in bigrams]
+    bigram_counts = Counter(bigrams)
+    print(bigram_counts.most_common(10))
+    return bigrams_col
 
 
 def find_topics():
@@ -45,4 +71,7 @@ if __name__ == "__main__":
                 "postprocessor/one-million-reddit-confessions-sample.csv")
     df = pd.read_csv(filepath)
     df['lemma'] = tokenize_text(df['title'])
+    df['bigrams'] = generate_ngrams(df['lemma'], 2)
+    
+    
     
