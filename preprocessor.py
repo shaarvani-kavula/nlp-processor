17efd5c8ab07146d4cff9fe2633ef2e9299c0edc
Shaarvani Kavula <123433000+shaarvani-kavula@users.noreply.github.com>
2023-01-29 23:10:50 -0600

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 29 22:58:23 2023
+
+@author: shaar
+"""
+
+import praw
+import pandas as pd
+from creds import client_id, client_secret, user_agent 
+
+reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,  user_agent=user_agent)
+    
+posts = []
+ml_subreddit = reddit.subreddit('MachineLearning')
+for post in ml_subreddit.hot(limit=10):
+    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+print(posts)
+
